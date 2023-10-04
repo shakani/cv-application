57964eae3e8e89ce0e004875425fff009904f490
Sami Hakani <sami.hakani@gmail.com>
2023-10-04 15:46:55 -0400

    Mocked biodata form

---
+++ cv-application/src/components/Biodata.jsx
@@ -0,0 +1,30 @@
+import '../styles/biodata.css'
+
+function Biodata() {
+    return (
+        <div>
+            <form>
+                <ul>
+                    <li>
+                        <label htmlFor='name'>Full Name</label>
+                        <input type='text' name='name'></input>
+                    </li>
+                    <li>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' name='email'></input>
+                    </li>
+                    <li>
+                        <label htmlFor='number'>Phone Number ()</label>
+                        <input type='tel' name='number'></input>
+                    </li>
+                    <li>
+                        <label htmlFor='address'>Address</label>
+                        <input type='text' name='address'></input>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    );
+}
+
+export default Biodata
